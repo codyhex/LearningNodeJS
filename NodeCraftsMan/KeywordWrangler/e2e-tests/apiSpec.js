b31785073df593f4fd5ce6efc52df0cbf2ee33b6
Peng He <phe@hstartech.com>
2016-08-12 11:46:47 -0400

    init the ws and setup the karma

---
+++ NodeCraftsMan/KeywordWrangler/e2e-tests/apiSpec.js
@@ -0,0 +1,11 @@
+/**
+ * Created by user on 8/12/16.
+ */
+'use strict';
+
+describe('The API', function () {
+
+    it('should just work', function () {
+        expect(true).toBe(true);
+    })
+});
